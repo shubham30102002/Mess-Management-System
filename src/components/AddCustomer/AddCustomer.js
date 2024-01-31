2e6578d4f7c7416ea4f2dd6bdf3f1a64708f2774
shubham30102002 <shubhamrg30@gmail.com>
2024-01-31 21:47:19 +0530

    add customer

--- src/components/AddCustomer/AddCustomer.js
+++ src/components/AddCustomer/AddCustomer.js
@@ -1,8 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Payment from './Payment';
+
 
 function AddCustomer() {
+  const [diningStatus, setDiningStatus] = useState(true);
+  const [tiffinStatus, setTiffinStatus] = useState(false);
+
+  const [showPayment, setShowPayment] = useState(false);
+
+  const handleDinigChange = () => {
+    setDiningStatus(true);
+    setTiffinStatus(false);
+  }
+
+  const handleTiffinChange = () => {
+    setDiningStatus(false);
+    setTiffinStatus(true);
+  }
   return (
-    <div>AddCustomer</div>
+
+    <div className="w-[95vw] h-[70vh] mx-auto mt-4 ">
+      <header className="bg-yellow-300 w-[95vw] h-[8vh] text-2xl font-bold py-2 px-10 rounded-tl-full ">
+        Add Customer
+      </header>
+
+      <div className="w-[95vw] h-[70vh]  mx-auto  bg-white ">
+        <button className=''>
+          Add Customer
+        </button>
+
+        <div >
+          <button className={`${diningStatus ? 'bg-yellow-300' : null}`}
+            onClick={handleDinigChange} >Dining</button>
+          <button className={`${tiffinStatus ? 'bg-yellow-300' : null}`}
+            onClick={handleTiffinChange}>Tiffin</button>
+        </div>
+        <form>
+          <div className='flex gap-4'>
+
+            <div>
+              <label>Name:</label>
+              <br />
+              <input className='bg-zinc-200 w-[40vw] border border-black outline-8' />
+              <br />
+
+              <label>Mobile Number:</label>
+              <br />
+              <input className='bg-zinc-200 w-[40vw] border border-black outline-8' />
+              <br />
+
+              <label>Permanent Address:</label>
+              <br />
+              <input className='bg-zinc-200 w-[40vw] border border-black outline-8' />
+              <br />
+
+              <label>Temporary Address:</label>
+              <br />
+              <input className='bg-zinc-200 w-[40vw] border border-black outline-8' />
+              <br />
+
+              <label>Aadhar Number:</label>
+              <br />
+              <input className='bg-zinc-200 w-[40vw] border border-black outline-8' />
+              <br />
+            </div>
+
+            <div>
+              <label>Plan:</label>
+              <br />
+              <input className='bg-zinc-200 w-[40vw] border border-black outline-8' />
+              <br />
+
+              {
+                diningStatus ? (
+                  <div>
+                    <label>FingerPrint/ Biometric Details:</label>
+                    <br />
+                    <div className='bg-zinc-200 w-[30vw] h-[25vh]'></div>
+                  </div>
+                ) : null
+              }
+
+            </div>
+
+           
+          </div>
+
+          <footer>
+              <div>
+                <button className="bg-yellow-400 px-8 border border-black outline-8  
+          font-semibold" onClick={() => setShowPayment(true)}>Continue</button>
+                <button className="bg-yellow-400 px-8 border border-black outline-8  
+          font-semibold">Close</button>
+
+              </div>
+
+            </footer>
+        </form>
+      </div>
+
+      {
+        showPayment ? <Payment/> : null
+      }
+    </div>
   )
 }
 
---
+++ src/components/AddCustomer/Payment.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Payment = () => {
+  return (
+    <div>
+        <div className='bg-yellow-300'>
+            Payment
+        </div>
+
+        {/* payment summary */}
+        <div>
+            <div>
+                <div>Category</div>
+                <div>Selected Plan</div>
+                <div>Total Payment = </div>
+            </div>
+
+
+
+            {/* QR  */}
+            <div>
+                
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Payment
